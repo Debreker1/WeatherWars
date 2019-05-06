a798c334b882a64442adafd49d442d6974b5d85d
Ryan Graute <rrgraute@gmail.com>
2019-05-06 14:04:17 +0200

    working web3

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,45 @@
 import * as React from 'react';
 import { Button } from '@material-ui/core/';
 import ResponsiveDrawer from '../../components/Header'
+import getWeb3 from '../../web3/getWeb3';
+import { Accounts } from 'web3-eth-accounts';
 
 
-class Home extends React.Component
+type Props = {};
+type State = {
+    web3: any,
+    accounts: any
+};
+
+class Home extends React.Component<Props, State>
 {
+
+    public async componentDidMount()
+    {
+        console.log("mounted");
+        this.state = {
+            web3: await getWeb3(),
+            accounts: []
+        }
+    }
+    
+    deployContract = async () => {
+        console.log("knop2");
+        const account = await this.state.web3.eth.getAccounts();
+        console.log(account);
+        this.setState({
+            ...this.state,
+            accounts: account
+        })
+    }
+
     public render()
     {
         return(
             <div>
                 <ResponsiveDrawer />
                 <p>Dit is de homepage</p>
-                <Button variant="contained" color="primary">Hello world</Button>
+                <Button onClick={this.deployContract} variant="contained" color="primary">Hello world</Button>
             </div>
         )
     }
