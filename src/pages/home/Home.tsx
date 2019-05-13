f3982b286332acf84a53a64fb6cb0b71a6d99769
Ryan Graute <rrgraute@gmail.com>
2019-05-13 18:29:03 +0200

    deploy contract working

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,14 +2,13 @@ import * as React from 'react';
 import { Button } from '@material-ui/core/';
 import ResponsiveDrawer from '../../components/Header'
 import getWeb3 from '../../web3/getWeb3';
-
-const contract = require("truffle-contract");
-const weatherContract = require("../../contracts/WeatherContract.json"); 
+import WeatherContract from "../../contracts/WeatherContract.json";
 
 type Props = {};
 type State = {
     web3: any,
-    accounts: any
+    accounts: any,
+    networkId: number
 };
 
 class Home extends React.Component<Props, State>
@@ -17,33 +16,38 @@ class Home extends React.Component<Props, State>
 
     public async componentDidMount()
     {
-        console.log("mounted");
-        this.state = {
-            web3: await getWeb3(),
-            accounts: []
+        try
+        {
+           const web3 = await getWeb3();
+           web3.eth.transactionConfirmationBlocks = 1;
+           const accounts = await web3.eth.getAccounts();
+           const networkId = await web3.eth.net.getId();
+
+           this.state = {
+            web3: web3,
+            accounts: accounts,
+            networkId: networkId
+            } 
+        }
+        catch(error)
+        {
+            console.error(error);
         }
     }
     
     deployContract = async () => {
-        console.log("knop2");
-        const account = await this.state.web3.eth.getAccounts();
-        console.log(account);
-        this.setState({
-            ...this.state,
-            accounts: account
-        })
-        this.artifactsToContract(weatherContract)
-        .then((WeatherContractAbstraction) => {
-            WeatherContractAbstraction.deployed().then((deployed : any) => {
-                console.log(deployed);
-            })
-        })
-    }
+        const account = this.state.accounts[0]
+        const nonce = await this.state.web3.eth.getTransactionCount(account);
+        const weathercontract = await new this.state.web3.eth.Contract(WeatherContract.abi);
+        const createdContract = weathercontract.deploy({
+            data: WeatherContract.bytecode
+        }).send({from: account})
+        .then((contract) => {
+            console.log("pay up!");
+            contract.methods.update().send({from: account})
+        });
 
-    artifactsToContract = async (artifacts : any) => {
-        const contractAbstraction = contract(artifacts);
-        contractAbstraction.setProvider(this.state.web3.currentProvider);
-        return contractAbstraction
+        // createdContract.methods.update().call();
     }
 
     public render()
