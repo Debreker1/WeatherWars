3f2583eb95fc56203c3a0ea873bf0aa039408aa8
Ryan Graute <rrgraute@gmail.com>
2019-06-03 10:11:36 +0200

    add pages

---
+++ src/pages/bets/BetDetails.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { connect } from 'react-redux'
+
+type Props = {};
+type State = {};
+
+class BetDetails extends React.Component<Props, State> {
+
+  public render() {
+    return (
+      <div>Dit is de pagina voor een specifieke bet</div>
+    )
+  }
+}
+
+export default BetDetails;
---
+++ src/pages/bets/BetsOverview.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { connect } from 'react-redux'
+
+type Props = {};
+type State = {};
+
+class BetsOverview extends React.Component<Props, State> {
+
+  public render() {
+    return (
+      <div>Dit is de bets overview page</div>
+    )
+  }
+}
+
+export default BetsOverview;
