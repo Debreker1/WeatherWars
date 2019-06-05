16a83fe56c17b6a87f2a8780e36487db256dc3bb
Ryan Graute <rrgraute@gmail.com>
2019-06-05 22:13:56 +0200

    start updating inputs and deploy function

--- src/pages/bets/AddBet.tsx
+++ src/pages/bets/AddBet.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import { connect } from 'react-redux'
 import WeatherBet from '../../contracts/BettingContract.json';
-import { Button, Icon } from '@material-ui/core';
-import Loadbutton from '../../components/LoadButton'
+import { Button, Icon, FormControl, InputLabel, TextField } from '@material-ui/core';
+import { KeyboardDateTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import Loadbutton from '../../components/LoadButton';
 import { Redirect } from 'react-router';
+import { format, differenceInSeconds } from 'date-fns';
 
 enum status {
   Add = "Add",
@@ -17,7 +20,8 @@ type State = {
   betAmount: string,
   status: status,
   redirect: boolean,
-  redirectLink: string
+  redirectLink: string,
+  date: Date | null
 };
 
 class AddBet extends React.Component<Props, State> {
@@ -28,61 +32,88 @@ class AddBet extends React.Component<Props, State> {
       betAmount: "1",
       status: status.Add,
       redirect: false,
-      redirectLink: ""
+      redirectLink: "",
+      date: new Date()
     }
   }
 
-  handleChange = (event) => {
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const name: string = event.target.name;
     const value: any = event.target.value;
     this.setState({
       ...this.state,
       [name]: value
     });
+    console.log(name + " : " + value);
   }
 
-  deployContract = async () => {
-    // e.preventDefault();
-    try {
-      this.setState({ status: status.Deploying });
-      const account = this.props.accounts[0];
-      const valueAmount = await this.props.web3.utils.toWei(this.state.betAmount, "ether")
-      const weatherContract = await new this.props.web3.eth.Contract(WeatherBet.abi);
-      const betDeploy = await weatherContract.deploy({
-        data: WeatherBet.bytecode,
-        arguments: [5, valueAmount]
-      });
-      // const gasAmount = await betDeploy.estimateGas({from: this.state.accounts[0], value: valueAmount});
-      const deployed = await betDeploy.send({
-        from: account,
-        value: valueAmount,
-        gas: 3000000
-      });
-      this.setState({status: status.Done, redirect: true, redirectLink: "/bets/" + deployed.options.address});
-    } catch (err) {
-      this.setState({ status: status.Error });
-      throw err;
+  handleDateChange = (date: Date | null) => {
+    this.setState({ date: date });
+  };
+
+  deployContract = async (e) => {
+    e.preventDefault();
+    if (this.state.date != null) {
+      try {
+        this.setState({ status: status.Deploying });
+        const amountOfSeconds = differenceInSeconds(new Date(), this.state.date);
+        const account = this.props.accounts[0];
+        const valueAmount = await this.props.web3.utils.toWei(this.state.betAmount, "ether")
+        const weatherContract = await new this.props.web3.eth.Contract(WeatherBet.abi);
+        const betDeploy = await weatherContract.deploy({
+          data: WeatherBet.bytecode,
+          arguments: [amountOfSeconds, valueAmount]
+        });
+        // const gasAmount = await betDeploy.estimateGas({from: this.state.accounts[0], value: valueAmount});
+        const deployed = await betDeploy.send({
+          from: account,
+          value: valueAmount,
+          gas: 3000000
+        });
+        this.setState({ status: status.Done, redirect: true, redirectLink: "/bets/" + deployed.options.address });
+      } catch (err) {
+        this.setState({ status: status.Error });
+        throw err;
+      }
     }
   };
 
   public render() {
-    return (
-      <div>
-        {this.state.redirect && <Redirect to={this.state.redirectLink}/>}
-        <p>Current status: {this.state.status}</p>
-        <h1>Add bet</h1>
-        <form className="createBet-login" onSubmit={this.deployContract}>
-          <div>
-            <label htmlFor="betAmount">Ethereum:</label>
-            <input name="betAmount" type="number" min="1" step="any" value={this.state.betAmount} onChange={this.handleChange} />
-          </div>
-          <div>
-            <Loadbutton onClick={this.deployContract}>Contract Aanmaken</Loadbutton>
-          </div>
-        </form>
-      </div>
-    )
+    return this.formLayout();
   }
+
+  formLayout = () => {
+    return (
+      <form>
+        <TextField
+          id="betAmount"
+          name="betAmount"
+          label="Bet Amount"
+          value={this.state.betAmount}
+          onChange={this.handleChange}
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          margin="normal"
+        />
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <KeyboardDateTimePicker
+            margin="normal"
+            id="mui-pickers-date"
+            name="date"
+            label="Datum en tijd van weer"
+            disablePast={true}
+            value={this.state.date}
+            onChange={this.handleDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+        </MuiPickersUtilsProvider>
+      </form>
+    );
+  };
 }
 
 interface stateProps {
